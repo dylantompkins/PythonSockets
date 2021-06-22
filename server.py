beec8097ea1d507f5fd87d691b8ecd21b54f6ded
Dylan Tompkins <dylanltompkins@gmail.com>
2021-06-21 21:02:57 -0400

    server moved to class

--- server.py
+++ server.py
@@ -4,16 +4,27 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 8888))
 serversocket.listen(5)
 
-clientsocket = None
+connection = None
+
+class Connection:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def recv(self):
+        byte = clientsocket.recv(1)
+        self.num = int.from_bytes(byte, "big", signed=True)
+
+    def display(self):
+        spaces = 45 + self.num
+        print(' ' * spaces + '|')
 
 while True:
-    if (clientsocket == None):
+    if (connection == None):
         (clientsocket, address) = serversocket.accept()
+        connection = Connection(clientsocket)
     else:
-        byte = clientsocket.recv(1)
-        num = int.from_bytes(byte, "big", signed=True)
-        spaces = 45 + num
-        print(' ' * spaces + '|')
+        connection.recv()
+        connection.display()
 
 # TODO
 # - threading for multiple clients
